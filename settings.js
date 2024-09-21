cf844a9690174c550fefecb76f2eed5657be4e95
lenjasa <len-ja@outlook.de>
2024-09-21 21:44:34 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,14 +12,14 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: KermHackTools-s9s" //ur yt chanel name
+global.ytname = "YT: leniiiis" //ur yt chanel name
 global.socialm = "GitHub: Kgtech-cmr" //ur github or insta name
 global.location = "Cameroon, Douala, Yaoundé" //ur location
 
 //new
-global.botname = 'Kerm Bug v2' //ur bot name
-global.ownernumber = ['237697517505'] //ur owner number, dont add more than one
-global.ownername = '🦄KgTech' //ur owner name
+global.botname = 'MrsAnthrop' //ur bot name
+global.ownernumber = ['491787297129'] //ur owner number, dont add more than one
+global.ownername = '🦄Lenlen' //ur owner name
 global.websitex = "https://youtu.be/tLq8_qOOGFI"
 global.wagc = "https://whatsapp.com/channel/0029Vafn6hc7DAX3fzsKtn45"
 global.themeemoji = '🪀'
@@ -29,7 +29,7 @@ global.packname = "Sticker By"
 global.author = "⚡️KgTech\n\n+237656520674"
 global.creator = "237656520674@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["237683236218"] // Premium User
+global.premium = ["491787297129"] // Premium User
 
 //channel id
 global.xchannel = {
